d13e670eb77a4455421f3f299a7fc04ca7b133c1
Konrad Fijałkowski <fijalkowskikonras@gmail.com>
2023-01-16 02:43:21 +0100

    funkcjonalność zmiany rozmiaru i translacji

---
+++ rysowanieASCII/rysowanie.cpp
@@ -0,0 +1,130 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <Windows.h>
+#include <stdio.h>
+
+void wybierzPoczatek(char &znak, int &rozmiar) {
+    std::cout << "Wpisz znak jakim chcesz rysowaæ: " << std::endl;
+    std::cin >> znak;
+    std::cout << "Podaj rozmiar figury: " << std::endl;
+    std::cin >> rozmiar;
+    system("CLS");
+}
+
+void zmienPozycjeKursora(HANDLE hStdout, int x, int y, int offsetX, int offsetY) {
+    SMALL_RECT srctWindow{};
+    CONSOLE_SCREEN_BUFFER_INFO info;
+
+    GetConsoleScreenBufferInfo(hStdout, &info);
+
+    int top = info.srWindow.Top;
+    int right = info.srWindow.Right;
+    COORD c;
+    c.X = right - x + offsetX;
+    c.Y = top + y -1 + offsetY;
+    //std::cout << c.X << " " << c.Y << std::endl;
+    SetConsoleCursorPosition(hStdout, c);
+}
+//Funkcja która faktycznie rysuje figure opiera sie na rozwiazaniu problemu "Ile pustych znaków musi pojawiæ sie miêdzy znakami ASCII? w poszczególnycg"
+//Innym podejœciem, ktore mo¿na zastosowaæ, jest rysowanie znaku tylko w wybranych miejscach.
+void rysujFigure(HANDLE hStdout, int rozmiaryFigury, char znak, int offsetX = 0, int offsetY = 0) {
+    int srodek;
+    int wiersz;
+
+    //sprawdzam parzystosc rozmiaru figury, któr¹ zamierzam narysowac
+    //ten test zdeterminuje jakie zmienne oraz ich wartosci mam przyjac podczas jej rysowania
+    if (rozmiaryFigury % 2 == 0) {
+        srodek = rozmiaryFigury / 2;
+
+        for (int wiersz = 1; wiersz < srodek; ++wiersz) {
+            zmienPozycjeKursora(hStdout, rozmiaryFigury, wiersz, offsetX, offsetY);
+            std::cout << std::string(wiersz - 1, ' ') << znak << std::string(rozmiaryFigury - (wiersz * 2), ' ') << znak << std::endl;
+        }
+        for (int i = 0; i < 2; ++i) {
+            zmienPozycjeKursora(hStdout, rozmiaryFigury, srodek + i, offsetX, offsetY);
+            std::cout << std::string(srodek - 1, ' ') << std::string(2, znak) << std::endl;
+        }
+
+
+        for (int wiersz = 1; wiersz < srodek; ++wiersz) {
+            zmienPozycjeKursora(hStdout, rozmiaryFigury, srodek + wiersz + 1, offsetX, offsetY);
+            std::cout << std::string(srodek - 1 - wiersz, ' ') << znak << std::string(wiersz * 2, ' ') << znak << std::endl;
+        }
+
+    }
+    else {
+        srodek = (rozmiaryFigury / 2) + 1;
+
+        for (int wiersz = 1; wiersz < srodek; ++wiersz) {
+            zmienPozycjeKursora(hStdout, rozmiaryFigury, wiersz, offsetX, offsetY);
+            std::cout << std::string(wiersz - 1, ' ') << znak << std::string(rozmiaryFigury - (wiersz * 2), ' ') << znak << std::endl;
+        }
+        zmienPozycjeKursora(hStdout, rozmiaryFigury, srodek, offsetX, offsetY);
+        std::cout << std::string(srodek - 1, ' ') << znak << std::endl;
+        for (int wiersz = 1; wiersz < srodek; ++wiersz) {
+            zmienPozycjeKursora(hStdout, rozmiaryFigury, srodek + wiersz, offsetX, offsetY);
+            std::cout << std::string(srodek - 1 - wiersz, ' ') << znak << std::string(wiersz * 2 - 1, ' ') << znak << std::endl;
+        }
+    }
+};
+void przywitanie() {
+
+}
+//funkcja pokazuj¹ca pomocne informacje dla uzytkownika
+void help() {
+
+}
+//bazowa funkcja s³u¿¹ca do stworzenia UI uzytkownika
+void menu(HANDLE whnd, HANDLE rhnd) {
+    char znak = 'x';
+    int rozmiar = 10;
+
+    DWORD events = 0;
+    DWORD eventsread = 0;
+
+    int offsetX = 0;
+    int offsetY = 0;
+    //wybierzPoczatek(znak, rozmiar);
+    rysujFigure(whnd, rozmiar, znak);
+    while (TRUE) {
+        GetNumberOfConsoleInputEvents(rhnd, &events);
+        if (events != 0) {
+            INPUT_RECORD* eventBuffer = new INPUT_RECORD[events];
+
+            ReadConsoleInput(rhnd, eventBuffer, events, &eventsread);
+            for (DWORD i = 0; i < eventsread; ++i) {
+                if (eventBuffer[i].EventType == KEY_EVENT && eventBuffer[i].Event.KeyEvent.bKeyDown) {
+                    system("CLS");
+                    switch (eventBuffer[i].Event.KeyEvent.wVirtualKeyCode)  {
+                        case VK_OEM_PLUS:
+                            rozmiar++;
+                            break;
+                        case VK_OEM_MINUS:
+                            if (rozmiar != 3) { rozmiar--; }
+                            break;
+                        case VK_UP:
+                            offsetY--;
+                            break;
+                        case VK_DOWN:
+                            offsetY++;
+                            break;
+                        case VK_RIGHT:
+                            offsetX++;
+                            break;
+                        case VK_LEFT:
+                            offsetX--;
+                            break;
+                        default:
+                            std::cout << "not handled yet" << std::endl;
+                            break;
+                    }
+                    rysujFigure(whnd, rozmiar, znak, offsetX, offsetY);
+
+                }
+
+            }
+        }
+    }
+}
--- rysowanieASCII/rysowanieASCII.cpp
+++ rysowanieASCII/rysowanieASCII.cpp
@@ -7,24 +7,20 @@ Założenia projektu:
 - punkt zaczepienia znajduje się w prawym górnym narożniku ekranu. check
 - wybór znaku kodu ASCII; check
 − wczytanie początkowych rozmiarów figury; check
-− zmianę wielkości figury klawiszami ′+′ i ′-′;
+− zmianę wielkości figury klawiszami ′+′ i ′-′; check
 − przesuwanie figury za pomocą kursorów ←, ↑, →, ↓;
 − ograniczenie przesuwania i rozmiarów figury do obszaru ekranu
 */
 
-char wybierzZnak();
-void zmienPozycjeKursora(HANDLE hStdout, int x, int y);
-void rysujFigure(HANDLE hStdout, int rozmiary, char znak);
-void menu(HANDLE hStdout);
-void przywitanie(), help();
 int main()
 {
     setlocale(LC_ALL, "");
     SetConsoleTitle(L"Rysownik figur");
     //Zaczynamy od stworzenia zmiennej pozwalającej zarządać właściwościami okna konsoli
     //potem jako parametr będą przyjmować je wszystkie funkcje
-    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-    menu(hStdout);
+    HANDLE whnd = GetStdHandle(STD_OUTPUT_HANDLE);
+    HANDLE rhnd = GetStdHandle(STD_INPUT_HANDLE);
+    menu(whnd, rhnd);
    
 
 }
